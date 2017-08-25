6b604b5b4ac74f99267c8a0032d4bc42a62c617e
Julia Finarovsky <juliafinarovsky@gmail.com>
2017-08-25 19:45:51 -0400

    expanded and organized user schema (user.js)

--- back/models/user.js
+++ back/models/user.js
@@ -3,55 +3,301 @@
  */
 
 
-const bcrypt = require("bcrypt"),
-    mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
 
-const schema = new mongoose.Schema({
+const emailValid = [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email.'];
+const phonenumberValid = [/(?=^\d{10}$)|(?=^\d{3}-\d{3}-\d{4}$)|(?=^\(\d{3}\)\d{3}-\d{4}$)/, 'Please enter a valid phone number in the format 1234567890, 123-456-7890, or (123)456-7890'];
+const zipcodeValid = [/^\d{5}(?:[-\s]\d{4})?$/, 'The zipcode must either be in the format XXXXX or XXXXX-XXXX.'];
 
-    local            : {
-        email        : {
-            type:String,
-            unique:true
-        },
-        password     : String
-    },
-    facebook         : {
-        id           : String,
-        token        : String,
-        email        : String,
-        name         : String
-    },
-    twitter          : {
-        id           : String,
-        token        : String,
-        displayName  : String,
-        username     : String
-    },
-    google           : {
-        id           : String,
-        token        : String,
-        email        : String,
-        name         : String
-    },
-
-    linkedin         : {
-        id           : String,
-        token        : String,
-        email        : String,
-        name         : String
-    }
+const addressSchema = new mongoose.Schema({
+  street_address: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  apartment_number: mongoose.Schema.Types.Mixed,
+  city: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  state: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 2
+  },
+  zipcode: {
+    type: String,
+    required: true,
+    match: zipcodeValid
+  },
+  _id: false
 });
 
-schema.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+
+const userSchema = new mongoose.Schema({
+
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+    default: "Enter your first name"
+  },
+
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+    default: "Enter your first name"
+  },
+
+  gender: {
+    type: String,
+    default: "Enter your gender",
+    match: [/M|F/]
+  },
+
+  phoneNumber: {
+    type: String,
+    required: true,
+    match: phonenumberValid,
+    trim: true,
+    default: "Enter your phone number"
+  },
+
+  address: {
+    type:addressSchema,
+    required: true
+  },
+
+  birthday: {
+    
+    month: {
+      type: String
+    },
+
+    day: {
+      type: String
+    },
+
+    year: {
+      type: String
+    }
+
+  },
+
+  photo: {
+    type: String
+  },
+
+  photoUrl: {
+    type: String
+  },
+
+  profiles: {
+    
+    github: {
+      type: String,
+      default: "Enter a link to your github profile"
+    },
+
+    linkedIn: {
+      type: String,
+      default: "Enter a link to your linkedIn profile"
+    },
+
+    website: {
+      type: String,
+      default: "Enter a link to your website"
+    }
+  },
+
+
+  // Any more skills to add to the list?
+  skills: {
+    languages: {
+
+    
+      HTML: {
+        type: Boolean
+      },
+      
+      CSS: {
+        type: Boolean
+      },
+
+      Javascript: {
+        type: Boolean
+      },
+
+      Java: {
+        type: Boolean
+      },
+
+      CPlusPlus: {
+        type: Boolean
+      },
+
+      Python: {
+        type: Boolean
+      },
+
+      PHP: {
+        type: Boolean
+      },
+
+      Perl: {
+        type: Boolean
+      },
+
+      Ruby: {
+        type: Boolean
+      },
+
+      Rust: {
+        type: Boolean
+      }
+
+    },
+
+    frameworksAndLibraries: {
+      Node: {
+        type: Boolean
+      },
+
+      Express: {
+        type: Boolean
+      },
+
+      React: {
+        type: Boolean
+      },
+
+      Angular: {
+        type: Boolean
+      },
+
+      Backbone: {
+        type: Boolean
+      },
+
+      Ember: {
+        type: Boolean
+      },
+
+      JQuery: {
+        type: Boolean
+      },
+
+      Polymer: {
+        type: Boolean
+      },
+
+      Knockout: {
+        type: Boolean
+      },
+
+      VueJS: {
+        type: Boolean
+      },
+
+      Bootstrap: {
+        type: Boolean
+      },
+
+      Flask: {
+        type: Boolean
+      },
+
+      ASPdotNET: {
+        type: Boolean
+      },
+
+      SASS: {
+        type: Boolean
+      },
+
+      LESS: {
+        type: Boolean
+      },
+
+
+    },
+
+    databases: {
+      SQL: {
+        type: Boolean
+      },
+
+      MongoDB: {
+        type: Boolean
+      }
+    }
+  },
+  
+  jobStatus: {
+    type: String,
+    default: "Enter your job status"
+  },
+
+  jobTitle: {
+    type: String,
+    default: "Enter your job title"
+  },
+
+  loginData: {
+    local: {
+      email: {
+        type: String,
+        unique: true,
+        match: emailValid,
+        minlength: 1
+      },
+      password: String
+    },
+    facebook: {
+      id: {type: String},
+      token: {type: String},
+      email: {type: String},
+      name: {type:String}
+    },
+    twitter: {
+      id: {type:String},
+      token: {type:String},
+      displayName: {type:String},
+      username: {type:String}
+    },
+    google: {
+      id: {type:String},
+      token: {type:String},
+      email: {type:String},
+      name: {type:String}
+    },
+
+    linkedin: {
+      id: {type:String},
+      token: {type: String},
+      email: {type: String},
+      name: {type:String}
+    }
+  }
+
+});
+
+userSchema.methods.generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 
 // checking if password is valid
-schema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.local.password);
+userSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.local.password);
 };
 
 
-const User = mongoose.model('User', schema);
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
