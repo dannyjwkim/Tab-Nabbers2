459a7fcbbce6ba38c08ed422dc600f831898fdb3
Esterling Accime <esterling@brainbytescreative.com>
2017-08-31 10:24:26 -0400

    Fully structured the Front End, and make it easier to develop

--- front/routes/routes.js
+++ front/routes/routes.js
@@ -1,17 +1,10 @@
 import React from "react";
 import { Route, IndexRoute, Router, browserHistory } from "react-router";
-
 import {Provider} from "react-redux";
-
 import Home from "../containers/_Home";
 import Video from "../components/home/video";
-
-
 import PropTypes from 'prop-types';
 import Pages from "./Pages";
-
-import EnsureLoggedInContainer from "./EnsureLoggedIn";
-
 
 
 const Routes = ({store}) => (
@@ -19,13 +12,8 @@ const Routes = ({store}) => (
     <Provider store={store}>
         <Router history={browserHistory}>
             <Route path="/" component={Home}>
-
                 {Object.keys(Pages).map((routing, i) => (<Route path={routing.toLowerCase()} component={Pages[routing]} key={i} />))}
-
-
-
                 <IndexRoute component={Video} />
-
             </Route>
         </Router>
     </Provider>
