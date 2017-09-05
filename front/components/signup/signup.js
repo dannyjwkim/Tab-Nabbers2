70e81fbd9a46cba580af37364694dee952410a21
Esterling Accime <esterling@brainbytescreative.com>
2017-09-04 23:13:01 -0400

    Fixing a few things

--- front/components/signup/signup.js
+++ front/components/signup/signup.js
@@ -6,11 +6,10 @@ import "../../public/css/home.scss";
 const Signup = () => {
 
     return (
-       
-            
+        <div className="container mainContainer">
 
-            <div className="container mainContainer">
-                <div className="container sideContainer">
+            <div className="container sideContainer">
+
                 <h1 className="leftHeading">Sign up</h1>
                 <input type="text" className="userName input" placeholder="Username"/>
                 <hr className="horizontalLine"/>
@@ -21,20 +20,24 @@ const Signup = () => {
                 <input type="password" className="repeatPassword input" placeholder="Retype Password"/>
                 <hr className="horizontalLine"/>
                 <h6 className="correct">Please Retype the Correct Password.</h6>
-                <button className="btn btn-info button" type="submit" name="action">Sign Up
-                <i className="material-icons right"></i>
-          </button>
-        </div>
-        <div className="container rightContainer">
-            <h1 className="rightHeading"><span className="up">Sign In with</span><br/>Social Network</h1>
-            <a href="/auth/facebook"><Button color="facebook"><Icon name="facebook"/>Log In with Facebook</Button></a>
-            <a href="/auth/twitter"><Button color = "twitter"><Icon name="twitter"/>Log In with Twitter</Button></a>
-            <a href="/auth/google"><Button color="google plus"><Icon name="google plus"/>Log In with Google+</Button></a>
-            <a href="/auth/linkedin"><Button color="linkedin"><Icon name="linkedin"/> Log In with LinkedIn</Button></a>
 
+                <button className="btn btn-info button" type="submit" name="action">Sign Up
+                    <i className="material-icons right"></i>
+                </button>
+
+            </div>
+
+            <div className="container rightContainer">
+                <h1 className="rightHeading"><span className="up">Sign In with</span><br/>Social Network</h1>
+                <a href="/auth/facebook"><Button color="facebook"><Icon name="facebook"/>Log In with Facebook</Button></a>
+                <a href="/auth/twitter"><Button color = "twitter"><Icon name="twitter"/>Log In with Twitter</Button></a>
+                <a href="/auth/google"><Button color="google plus"><Icon name="google plus"/>Log In with Google+</Button></a>
+                <a href="/auth/linkedin"><Button color="linkedin"><Icon name="linkedin"/> Log In with LinkedIn</Button></a>
+
+            </div>
+
+            <button className="circle"><h3>OR</h3></button>
         </div>
-        <button className="circle"><h3>OR</h3></button>
-    </div>
 
        
 
@@ -43,26 +46,3 @@ const Signup = () => {
 
 
 export default Signup;
-
-// <a href="/auth/facebook">
-//                 <Button color='facebook'>
-//                     <Icon name='facebook' /> Facebook
-//                 </Button>
-//             </a>
-
-
-//             <a href="/auth/linkedin">
-//                 <Button color='linkedin'>
-//                     <Icon name='linkedin' /> Linkedin
-//                 </Button>
-//             </a>
-//             <a href="/auth/twitter">
-//                 <Button color='twitter'>
-//                     <Icon name='twitter' /> Twitter
-//                 </Button>
-//             </a>
-//             <a href="/auth/google">
-//                 <Button color='google plus'>
-//                     <Icon name='google plus' /> Google
-//                 </Button>
-//             </a>
