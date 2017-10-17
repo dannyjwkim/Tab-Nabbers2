11350fc16d9ca0e79d98ebdaf6caf55085f1c8ba
Juliafin <juliafinarovsky@gmail.com>
2017-10-17 03:36:49 -0400

    fixed component tests

--- front/reducers/meetupReducers.js
+++ front/reducers/meetupReducers.js
@@ -1,7 +1,7 @@
 import {ADD_EVENT} from "../actions/actionTypes";
 
 
-export default function meetupReducer (state = [], action) {
+export default function meetupReducer (state = {events:[]}, action) {
     switch(action.type){
         case ADD_EVENT:
             return( 
