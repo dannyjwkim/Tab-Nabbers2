b422d9938da8d8b2037f3b79ecbb7756c9ab349f
Esterling Accime <esterling@brainbytescreative.com>
2017-10-22 21:05:55 -0400

    Fixing mongo issue

--- back/config/mongo.js
+++ back/config/mongo.js
@@ -1,4 +1,4 @@
 module.exports.TEST_DATABASE_URL = {
 
   url:
-  process.env.TEST_DATABASE_URL || 'mongodb://testAccount:password@ds161483.mlab.com:61git pgit 483/tab_nabbers_test_db'} 
+  process.env.TEST_DATABASE_URL || 'mongodb://testAccount:password@ds161483.mlab.com:61483/tab_nabbers_test_db'}
