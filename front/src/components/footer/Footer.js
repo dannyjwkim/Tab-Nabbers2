89f297b3d29910894af4f0eec246ec8b5dd81322
andrejarboe <andrejarboe@gmail.com>
2018-02-23 11:14:16 -0500

    changed insite links to routes with react router dom Link

--- front/src/components/footer/Footer.js
+++ front/src/components/footer/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 import "./footer.css";
 
 
@@ -6,18 +7,18 @@ const Footer = () => (
     <footer className="flex between">
         <div>
             <ul className="flex center">
-                <a href="https://github.com/Tab-nabbers/Tab-Nabbers2"><li> Github</li></a> 
-                <a href="#"><li> About</li></a> 
-                <a href="#"><li> Link</li></a> 
+                <a href="https://github.com/Tab-nabbers/Tab-Nabbers2" target='_blank' rel="noopener noreferrer"><li> Github</li></a>
+                <Link to = "/about"><li>About</li></Link> 
+                <Link to = "/link"><li>Link</li></Link>                 
             </ul>
         </div>
 
 
         <div>
             <ul className = "flex center">
-                <a href="#"><li className = "fa fa-twitter"></li></a>                                 
-                <a href="https://github.com/Tab-nabbers/Tab-Nabbers2"><li className = "fa fa-github"></li></a>
-                <a href="#"><li className = "fa fa-facebook-official"></li></a>                   
+                <a href="" target='_blank' rel="noopener noreferrer"><li className = "fa fa-twitter"></li></a>                                 
+                <a href="https://github.com/Tab-nabbers/Tab-Nabbers2" target='_blank' rel="noopener noreferrer"><li className = "fa fa-github"></li></a>
+                <a href="" target='_blank' rel="noopener noreferrer"><li className = "fa fa-facebook-official"></li></a>                   
                 
             </ul>
         </div>
